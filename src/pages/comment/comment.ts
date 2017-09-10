b405e1f8cd723a7b9587c72706b23f5ae5bb6a1f
healer12 <asus12121996@gmail.com>
2017-09-10 14:43:49 +0700

    Edit page comment !!

--- src/pages/comment/comment.ts
+++ src/pages/comment/comment.ts
@@ -6,9 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'comment.html'
 })
 export class CommentPage {
-
+  comment=[];
   constructor(public navCtrl: NavController) {
+    this.comment=[
+      {name:'Naruto', img:'naruto.jpg', cmt:'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.'},
+      {name:'Sasuke', img:'sasuke.jpg', cmt:'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.'},
+      {name:'Rock Lee', img:'lee.jpg', cmt:'Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.'}
 
+    ];
+  }
+  openNavCommentPage(item) {
+    this.navCtrl.push(CommentPage, { item: item });
   }
 
 }
