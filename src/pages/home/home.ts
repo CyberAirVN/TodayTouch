1d6364212bac07f486a155d867ee58b00622d778
healer12 <asus12121996@gmail.com>
2017-09-13 23:15:24 +0700

    Complete search button!!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,10 +9,17 @@ import {DetailsPage} from '../detail/detail';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  toggled: boolean;
+  searchTerm: String = '';
+  items: string[];
+  shownGroup = null;
   @ViewChild('pageSlider') pageSlider: Slides;
   tabs:any='0';
+
+  
   constructor(public navCtrl: NavController) {
-    
+    this.toggled = false;
+    this.initializeItems();  
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad SwipeTabsPage');
@@ -27,4 +34,47 @@ export class HomePage {
   openNavDetailsPage(item) {
     this.navCtrl.push(DetailsPage, { item: item });
   }
+  toggleSearch(group) {
+    this.toggled = this.toggled ? false : true;
+    if (this.isGroupShown(group)) {
+      this.shownGroup = null;
+      document.getElementById("display").style.display="none";
+      
+  } else {
+      this.shownGroup = group;
+     document.getElementById("display").style.display="block";
+  }
+
+}
+
+initializeItems() {
+    this.items = ['Amsterdam','Bogota','Mumbai','San José','Salvador']; 
+ }    
+
+triggerInput( ev: any ) {
+    // Reset items back to all of the items
+    this.initializeItems();
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }  
+}
+// toggleGroup(group) {
+//     if (this.isGroupShown(group)) {
+//         this.shownGroup = null;
+//         document.getElementById("display").style.display="none";
+        
+//     } else {
+//         this.shownGroup = group;
+//        document.getElementById("display").style.display="block";
+//     }
+    
+// };
+isGroupShown(group) {
+    return this.shownGroup === group;
+};
 }
